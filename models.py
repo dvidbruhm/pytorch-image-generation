9fd0e4681d01dbc0ac5419f735fbae9c5688b685
David <david.abraham.1336@gmail.com>
2018-05-16 15:54:13 -0700

    Add models for 64x64 images.

--- models.py
+++ models.py
@@ -9,9 +9,9 @@ def weights_init_general(model, mean, std):
             model._modules[m].bias.data.zero_()
 
 
-class Generator(nn.Module):
+class Generator32(nn.Module):
     def __init__(self, input_size, general_complexity, dropout_prob, weights_mean, weights_std):
-        super(Generator, self).__init__()
+        super(Generator32, self).__init__()
 
         self.loss = nn.BCELoss()
 
@@ -50,9 +50,9 @@ class Generator(nn.Module):
     def weights_init(self, mean, std):
         weights_init_general(self, mean, std)
 
-class Discriminator(nn.Module):
+class Discriminator32(nn.Module):
     def __init__(self, general_complexity, weights_mean, weights_std, packing):
-        super(Discriminator, self).__init__()
+        super(Discriminator32, self).__init__()
 
         self.loss = nn.BCELoss()
 
@@ -72,6 +72,95 @@ class Discriminator(nn.Module):
         )
         self.layer4 = nn.Sequential(
             nn.Conv2d(general_complexity * 4, 1, 4, 1, 0, bias=False),
+            nn.Sigmoid()
+        )
+    
+        self.weights_init(weights_mean, weights_std)
+
+    def forward(self, input):
+        output = self.layer1(input)
+        output = self.layer2(output)
+        output = self.layer3(output)
+        output = self.layer4(output)
+        return output
+
+    def weights_init(self, mean, std):
+        weights_init_general(self, mean, std)
+
+class Generator64(nn.Module):
+    def __init__(self, input_size, general_complexity, dropout_prob, weights_mean, weights_std):
+        super(Generator64, self).__init__()
+
+        self.loss = nn.BCELoss()
+
+        self.layer1 = nn.Sequential(
+            nn.ConvTranspose2d(input_size, 8 * 3 * general_complexity, 4, 1, 0, bias=False),
+            nn.BatchNorm2d(8 * 3 * general_complexity),
+            nn.ReLU(True),
+            nn.Dropout2d(dropout_prob)
+        )
+        self.layer2 = nn.Sequential(
+            nn.ConvTranspose2d(8 * 3 * general_complexity, 4 * 3 * general_complexity, 4, 1, 0, bias=False),
+            nn.BatchNorm2d(4 * 3 * general_complexity),
+            nn.ReLU(True),
+            nn.Dropout2d(dropout_prob)
+        )
+        self.layer3 = nn.Sequential(
+            nn.ConvTranspose2d(4 * 3 * general_complexity, 2 * 3 * general_complexity, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(2 * 3 * general_complexity),
+            nn.ReLU(True),
+            nn.Dropout2d(dropout_prob)
+        )
+        self.layer4 = nn.Sequential(
+            nn.ConvTranspose2d(2 * 3 * general_complexity, 1 * 3 * general_complexity, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(1 * 3 * general_complexity),
+            nn.ReLU(True),
+            nn.Dropout2d(dropout_prob)
+        )
+        self.layer5 = nn.Sequential(
+            nn.ConvTranspose2d(1 * 3 * general_complexity, 1 * 3, 4, 2, 1, bias=False),
+            nn.Tanh()
+        )
+
+        self.weights_init(weights_mean, weights_std)
+    
+    def forward(self, input):
+        output = self.layer1(input)
+        output = self.layer2(output)
+        output = self.layer3(output)
+        output = self.layer4(output)
+        return output
+
+    def weights_init(self, mean, std):
+        weights_init_general(self, mean, std)
+
+class Discriminator64(nn.Module):
+    def __init__(self, general_complexity, weights_mean, weights_std, packing):
+        super(Discriminator64, self).__init__()
+
+        self.loss = nn.BCELoss()
+
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(3 * packing, general_complexity, 4, 2, 1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True)
+        )
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(general_complexity, general_complexity * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(general_complexity * 2),
+            nn.LeakyReLU(0.2, inplace=True)
+        )
+        self.layer3 = nn.Sequential(
+            nn.Conv2d(general_complexity * 2, general_complexity * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(general_complexity * 4),
+            nn.LeakyReLU(0.2, inplace=True)
+        )
+        self.layer4 = nn.Sequential(
+            nn.Conv2d(general_complexity * 4, general_complexity * 8, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(general_complexity * 8),
+            nn.LeakyReLU(0.2, inplace=True)
+        )
+        self.layer5 = nn.Sequential(
+            nn.Conv2d(general_complexity * 8, 1, 4, 1, 0, bias=False),
             nn.Sigmoid()
         )
     
