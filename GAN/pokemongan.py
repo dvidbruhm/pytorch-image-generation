49473c37143994b466a06159a23fe7f9ac932a67
David <david.abraham.1336@gmail.com>
2018-05-25 13:24:01 -0700

    Minor change

--- GAN/pokemongan.py
+++ GAN/pokemongan.py
@@ -1,23 +1,24 @@
 from trainer import DCGANTrainer
-from utils import *
+import utils
+from hyperparameters import DATASET_NAME
 
 def main():
     # Create trainer for the DCGAN
     trainer = DCGANTrainer()
 
     # Save the hyperparameters used for this training
-    save_parameters(trainer.save_path)
+    utils.save_parameters(trainer.save_path)
 
     # Load the dataset
-    trainer.load_dataset()
+    trainer.load_dataset(DATASET_NAME)
     #trainer.test_load_mnist()
 
     # Start the training process
     trainer.train()
 
     # Save models
-    save_model(trainer.generator, trainer.save_path, "generator_end")
-    save_model(trainer.discriminator, trainer.save_path, "discriminator_end")
+    utils.save_model(trainer.generator, trainer.save_path, "generator_end")
+    utils.save_model(trainer.discriminator, trainer.save_path, "discriminator_end")
 
 if __name__ == "__main__":
     main()
