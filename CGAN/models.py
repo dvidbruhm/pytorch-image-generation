6e4d5346ce73f7894a96fe7b56700acc8d7256dc
David Abraham <david.abraham.1336@gmail.com>
2018-05-29 16:10:51 -0700

    First running version of CGAN

--- CGAN/models.py
+++ CGAN/models.py
@@ -3,59 +3,62 @@ import utils
 import torch
 import torch.nn as nn
 
-class Generator32(nn.Module):
-    def __init__(self, latent_size, general_complexity, dropout_prob, weights_mean, weights_std, image_channels, num_labels, image_size):
-        super(Generator32, self).__init__()
+class Generator(nn.Module):
+    def __init__(self, latent_size, general_complexity, dropout_prob, weights_mean, weights_std, image_channels, num_labels, image_size, label_latent_input):
+        super(Generator, self).__init__()
 
         self.loss = nn.BCELoss()
         
         self.image_size = image_size
         self.image_channels = image_channels
+        self.general_complexity = general_complexity
 
-        input_size = latent_size
-
-        self.layer1 = nn.Sequential(
-            nn.ConvTranspose2d(input_size, 4 * image_channels * general_complexity, 4, 1, 0, bias=False),
-            nn.BatchNorm2d(4 * image_channels * general_complexity),
-            nn.ReLU(True),
-            nn.Dropout2d(dropout_prob)
+        self.label_fc_layer = nn.Sequential(
+            nn.Linear(num_labels, label_latent_input),
+            nn.ReLU(True)
         )
+        self.fc_layer1 = nn.Sequential(
+            nn.Linear(latent_size + label_latent_input, 4 * image_channels * general_complexity * image_size * image_size),
+            nn.ReLU(True)
+        )        
         self.layer2 = nn.Sequential(
-            nn.ConvTranspose2d(4 * image_channels * general_complexity, 2 * image_channels * general_complexity, 4, 2, 1, bias=False),
+            nn.ConvTranspose2d(4 * image_channels * general_complexity, 2 * image_channels * general_complexity, 3, 1, 1, bias=False),
             nn.BatchNorm2d(2 * image_channels * general_complexity),
             nn.ReLU(True),
             nn.Dropout2d(dropout_prob)
         )
         self.layer3 = nn.Sequential(
-            nn.ConvTranspose2d(2 * image_channels * general_complexity, 1 * image_channels * general_complexity, 4, 2, 1, bias=False),
+            nn.ConvTranspose2d(2 * image_channels * general_complexity, 1 * image_channels * general_complexity, 3, 1, 1, bias=False),
             nn.BatchNorm2d(1 * image_channels * general_complexity),
             nn.ReLU(True),
             nn.Dropout2d(dropout_prob)
-        )
-        self.fc_layer4 = nn.Sequential(
-            nn.Linear(image_channels * general_complexity * 16 * 16, image_channels * image_size * image_size),
+        )        
+        self.layer4 = nn.Sequential(
+            nn.ConvTranspose2d(1 * image_channels * general_complexity, 1 * image_channels, 3, 1, 1, bias=False),
             nn.Tanh()
         )
-        self.label_fc_layer = nn.Sequential(
-            nn.Linear()
-        )
-
+        
         utils.weights_init_general(self, weights_mean, weights_std)
     
-    def forward(self, input):
+    def forward(self, input, label):
         current_batch_size = input.shape[0]
-        output = self.layer1(input)
+
+        label = self.label_fc_layer(label)
+
+        input = input.view(current_batch_size, -1)
+        input = torch.cat([input, label], 1)
+        
+        output = self.fc_layer1(input)
+        output = output.view(current_batch_size, 4 * self.general_complexity * self.image_channels, self.image_size, self.image_size)
         output = self.layer2(output)
         output = self.layer3(output)
-        output = output.view(current_batch_size, -1)
-        output = self.fc_layer4(output)
-        output = output.view(current_batch_size, self.image_channels, self.image_size, self.image_size)
+        output = self.layer4(output)
         return output
 
 
-class Discriminator32(nn.Module):
-    def __init__(self, general_complexity, weights_mean, weights_std, packing, image_channels, num_labels, image_size):
-        super(Discriminator32, self).__init__()
+class Discriminator(nn.Module):
+    def __init__(self, general_complexity, weights_mean, weights_std, packing, image_channels, num_labels, image_size, label_latent_input):
+        super(Discriminator, self).__init__()
 
         self.loss = nn.BCELoss()
 
@@ -75,8 +78,12 @@ class Discriminator32(nn.Module):
             nn.BatchNorm2d(general_complexity * 4),
             nn.LeakyReLU(0.2, inplace=True)
         )
+        self.label_fc_layer = nn.Sequential(
+            nn.Linear(num_labels, label_latent_input),
+            nn.ReLU(True)
+        )
         self.fc_layer4 = nn.Sequential(
-            nn.Linear(4 * general_complexity * image_size * image_size, 8 * general_complexity),
+            nn.Linear(4 * general_complexity * image_size * image_size + label_latent_input, 8 * general_complexity),
             nn.ReLU(True),
             nn.Linear(8 * general_complexity, 1),
             nn.Sigmoid()
@@ -84,11 +91,13 @@ class Discriminator32(nn.Module):
     
         utils.weights_init_general(self, weights_mean, weights_std)
             
-    def forward(self, input):
+    def forward(self, input, label):
         current_batch_size = input.shape[0]
         output = self.layer1(input)
         output = self.layer2(output)
         output = self.layer3(output)
         output = output.view(current_batch_size, -1)
+        label = self.label_fc_layer(label)
+        output = torch.cat([output, label], 1)
         output = self.fc_layer4(output)
         return output
--- CGAN/trainer.py
+++ CGAN/trainer.py
@@ -8,7 +8,7 @@ from torchvision import datasets
 import torchvision.transforms as transforms
 
 from hyperparameters import *
-from models import Generator32 as Generator, Discriminator32 as Discriminator
+from models import Generator, Discriminator
 
 class CGANTrainer():
     def __init__(self, save_path=SAVE_PATH, beta1=BETA1, beta2=BETA2, 
@@ -17,7 +17,8 @@ class CGANTrainer():
                  model_complexity=COMPLEXITY, learning_rate=LEARNING_RATE, packing=PACKING,
                  real_label_smoothing=REAL_LABEL_SMOOTHING, fake_label_smoothing=FAKE_LABEL_SMOOTHING,
                  dropout_prob=DROPOUT_PROB, nb_discriminator_step=NB_DISCRIMINATOR_STEP, 
-                 image_channels=IMAGE_CHANNELS, batch_size=MINIBATCH_SIZE, num_labels=NUMBER_LABELS):
+                 image_channels=IMAGE_CHANNELS, batch_size=MINIBATCH_SIZE, num_labels=NUMBER_LABELS,
+                 label_latent_input=LABEL_LATENT_INPUT):
 
         self.num_labels = num_labels
         self.batch_size = batch_size
@@ -35,8 +36,8 @@ class CGANTrainer():
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
         # Models
-        self.generator = Generator(latent_input, model_complexity, dropout_prob, weights_mean, weights_std, image_channels, num_labels, image_size).to(self.device)
-        self.discriminator = Discriminator(model_complexity, weights_mean, weights_std, packing, image_channels, num_labels, image_size).to(self.device)
+        self.generator = Generator(latent_input, model_complexity, dropout_prob, weights_mean, weights_std, image_channels, num_labels, image_size, label_latent_input).to(self.device)
+        self.discriminator = Discriminator(model_complexity, weights_mean, weights_std, packing, image_channels, num_labels, image_size, label_latent_input).to(self.device)
 
         # Optimizers
         self.D_optimiser = optim.Adam(self.discriminator.parameters(), lr = learning_rate, betas = (beta1, beta2))
@@ -45,7 +46,10 @@ class CGANTrainer():
         self.generator_losses = []
         self.discriminator_losses = []
 
-        self.saved_latent_input = torch.randn((nb_image_to_gen, latent_input, 1, 1)).to(self.device)
+        self.saved_latent_input = torch.randn((nb_image_to_gen * nb_image_to_gen, latent_input, 1, 1)).to(self.device)
+        self.saved_label = torch.zeros((nb_image_to_gen * nb_image_to_gen, num_labels))
+        for i in range(nb_image_to_gen):
+            self.saved_label[i*nb_image_to_gen:i*nb_image_to_gen+nb_image_to_gen][i] = 1
 
         # Create directory for the results if it doesn't already exists
         import os
@@ -96,7 +100,7 @@ class CGANTrainer():
                                                         label_fake_smooth if self.fake_label_smoothing else label_fake)
 
                 ### Train generator
-                loss_generator = self.train_generator(current_batch_size, label_real)
+                loss_generator = self.train_generator(current_batch_size, one_hot_label, label_real)
 
                 ### Keep track of losses
                 d_loss.append(loss_discriminator_total.item())
@@ -105,7 +109,7 @@ class CGANTrainer():
             self.discriminator_losses.append(torch.mean(torch.tensor(d_loss)))
             self.generator_losses.append(torch.mean(torch.tensor(g_loss)))
 
-            utils.save_images(self.generator(self.saved_latent_input), self.save_path + "gen_", self.image_size, self.image_channels, epoch)
+            utils.save_images(self.generator(self.saved_latent_input, self.saved_label), self.save_path + "gen_", self.image_size, self.image_channels, self.nb_image_to_gen, epoch)
 
             utils.write_loss_plot(self.generator_losses, "G loss", self.save_path, clear_plot=False)
             utils.write_loss_plot(self.discriminator_losses, "D loss", self.save_path, clear_plot=True)
@@ -117,19 +121,19 @@ class CGANTrainer():
         
         # Generate with noise
         latent_noise = torch.randn(current_batch_size, self.latent_input, 1, 1, device=self.device)
-        generated_batch = self.generator(latent_noise)
+        generated_batch = self.generator(latent_noise, one_hot_label)
         fake_data = utils.pack(generated_batch, self.packing)
 
         ### Train discriminator
         self.discriminator.zero_grad()
 
         # Train on real data
-        real_prediction = self.discriminator(real_data).squeeze()
+        real_prediction = self.discriminator(real_data, one_hot_label).squeeze()
         loss_discriminator_real = self.discriminator.loss(real_prediction, real_label)
         #loss_discriminator_real.backward()
 
         # Train on fake data
-        fake_prediction = self.discriminator(fake_data.detach()).squeeze()
+        fake_prediction = self.discriminator(fake_data.detach(), one_hot_label).squeeze()
         loss_discriminator_fake = self.discriminator.loss(fake_prediction, fake_label)
         #loss_discriminator_fake.backward()
 
@@ -141,17 +145,17 @@ class CGANTrainer():
         return loss_discriminator_total
 
 
-    def train_generator(self, current_batch_size, real_label):
+    def train_generator(self, current_batch_size, one_hot_label, real_label):
 
         # Generate with noise
         latent_noise = torch.randn(current_batch_size, self.latent_input, 1, 1, device=self.device)
-        generated_batch = self.generator(latent_noise)
+        generated_batch = self.generator(latent_noise, one_hot_label)
         fake_data = utils.pack(generated_batch, self.packing)
 
         ### Train generator
         self.generator.zero_grad()
 
-        fake_prediction = self.discriminator(fake_data).squeeze()
+        fake_prediction = self.discriminator(fake_data, one_hot_label).squeeze()
 
         # Loss
         loss_generator = self.generator.loss(fake_prediction, real_label)
